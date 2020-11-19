3b6c3dad32e09af6480c0b64948dbaf061c44b0b
Binjita Kandel Baral <binjita.baral@gmail.com>
2020-11-18 21:51:54 -0600

    created methods using JSX

--- src/App.js
+++ src/App.js
@@ -3,11 +3,20 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <h1> Hello from React</h1>
-      </div>
-    );
+
+    //  method 1(without JSX)
+    // return (
+    //   //  important attribute is className  and htmlFor "for"
+    //   <div className="App">
+    //     <h1> Hello from React</h1>
+    //   </div>
+    // );
+
+    //    method 2(using JXS)
+        return React.createElement('div', { className: 'App'},
+            React.createElement('h1', null, 'Hello From React'));
+
+  //      both methods are same but the way of writing code is different
   }
 }
 
